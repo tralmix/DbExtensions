8bb9ce06d4ffd031fe07fe7fc5b19c4641851b44
Alex Uhling <alex.uhling@gmail.com>
2021-10-05 21:09:56 -0400

    Initial commit

---
+++ DbExtensions/DbCommandExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System.Data.Common
+{
+    public static class DbCommandExtensions
+	{
+
+		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
+		{
+			return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts, 1);
+		}
+
+		private static async Task<DbDataReader> ExecuteReaderWithRetryAsync(DbCommand dbCommand, int retryAttempts, int attemptNumber)
+		{
+			try
+			{
+				return await dbCommand.ExecuteReaderAsync();
+			}
+			catch (Exception)
+			{
+				if (retryAttempts <= 0) throw;
+
+				await Task.Delay(((int)Math.Pow(2, attemptNumber)) * 1000);
+				return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts--, attemptNumber++);
+			}
+		}
+	}
+}
