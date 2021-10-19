29e5fd2ea416a0850fb975142521db9d97427738
Alex Uhling <alex.uhling@gmail.com>
2021-10-19 08:19:52 -0400

    Updated db command extensions and documentation

--- src/DbExtensions/DbCommandExtensions.cs
+++ src/DbExtensions/DbCommandExtensions.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace System.Data.Common
 {
     public static class DbCommandExtensions
 	{
+		private static int _defaultRetryAttempts = 1;
+		private static int _firstAttempt = 1;
+		private static int _maxExponent = 8;
+		
         ///<summary>
-        /// Will attempt to run <cref>DbCommand.ExecuteReaderAsync()</cref> up to <paramref name="retryAttempts"> times.
+        /// Recursively attempts to run <cref>DbCommand.ExecuteReaderAsync()</cref> with one retry attempt.
+        ///</summary>
+		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand)
+		{
+			return await dbCommand.ExecuteReaderWithRetryAsync(_defaultRetryAttempts);
+		}
+
+        ///<summary>
+        /// Recursively attempts to run <cref>DbCommand.ExecuteReaderAsync()</cref> up to <paramref name="retryAttempts"> times.
         /// Calls will back off exponentionally at a rate of 2^n up to n=8.
         ///</summary>
         ///<param name="retryAttempts">Number of attempts before exception thrown.</param>
 		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
 		{
-			return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts, 1);
+			return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts, _firstAttempt);
 		}
 
 		private static async Task<DbDataReader> ExecuteReaderWithRetryAsync(DbCommand dbCommand, int retryAttempts, int attemptNumber)
@@ -26,7 +36,7 @@ namespace System.Data.Common
 			{
 				if (retryAttempts <= 0) throw;
 
-				await Task.Delay(((int)Math.Pow(2, Math.Min(attemptNumber, 8)) * 1000));
+				await Task.Delay(((int)Math.Pow(2, Math.Min(attemptNumber, _maxExponent)) * 1000));
 				return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts--, attemptNumber++);
 			}
 		}
