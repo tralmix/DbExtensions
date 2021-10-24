3d1aed37af2d7558a6aa6614120d0ea7138812e2
Alex Uhling <alex.uhling@gmail.com>
2021-10-24 10:39:09 -0400

    Added synchronous example of retry code

--- examples/SynchronousExample/Program.cs
+++ examples/SynchronousExample/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System.Data.Common;
+using System.Diagnostics;
 
 namespace SynchronousExample
 {
     class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	{
+		static readonly string inMemoryConnectionString = "Data Source=InMemorySample;Mode=Memory;Cache=Shared";
+		static void Main(string[] args)
+		{
+			using var connection = new SqliteConnection(inMemoryConnectionString);
+
+			connection.EnsureOpenWithRetry(2);
+
+			using var command = connection.CreateCommand();
+
+			using var reader = command.ExecuteReaderWithRetry(2);
+
+			Debug.Assert(true);
+		}
+	}
 }
