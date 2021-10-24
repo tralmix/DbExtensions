5f4010bcc5e7c8d4619ca425a11c5f87fd1d75fa
Alex Uhling <alex.uhling@gmail.com>
2021-10-24 09:02:04 -0400

    Added overrides to DbCommand extensions. Updated intellisense descriptions.

--- src/DbExtensions/DbCommandExtensions.cs
+++ src/DbExtensions/DbCommandExtensions.cs
@@ -1,43 +1,65 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
 namespace System.Data.Common
 {
-    public static class DbCommandExtensions
+	public static class DbCommandExtensions
 	{
-		private static int _defaultRetryAttempts = 1;
-		private static int _firstAttempt = 1;
-		private static int _maxExponent = 8;
-		
-        ///<summary>
-        /// Recursively attempts to run <cref>DbCommand.ExecuteReaderAsync()</cref> with one retry attempt.
-        ///</summary>
+		private static readonly int _defaultRetryAttempts = 1;
+		private static readonly int _firstAttempt = 1;
+		private static readonly int _maxExponent = 8;
+
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteReaderAsync()"/> with one retry attempt.
+		///</summary>
 		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand)
 		{
 			return await dbCommand.ExecuteReaderWithRetryAsync(_defaultRetryAttempts);
 		}
 
-        ///<summary>
-        /// Recursively attempts to run <cref>DbCommand.ExecuteReaderAsync()</cref> up to <paramref name="retryAttempts"> times.
-        /// Calls will back off exponentionally at a rate of 2^n up to n=8.
-        ///</summary>
-        ///<param name="retryAttempts">Number of attempts before exception thrown.</param>
-		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
+		/// <summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteReaderAsync()"/> with one retry attempt.
+		/// </summary>
+		/// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+		/// <returns></returns>
+		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, CancellationToken cancellationToken)
 		{
-			return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts, _firstAttempt);
+			return await dbCommand.ExecuteReaderWithRetryAsync(_defaultRetryAttempts, cancellationToken);
 		}
 
-		private static async Task<DbDataReader> ExecuteReaderWithRetryAsync(DbCommand dbCommand, int retryAttempts, int attemptNumber)
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteReaderAsync()"/> up to <paramref name="retryAttempts"/> times.
+		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
+		///</summary>
+		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
+		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
+		{
+			return await dbCommand.ExecuteReaderWithRetryAsync(retryAttempts, _firstAttempt);
+		}
+
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteReaderAsync()"/> up to <paramref name="retryAttempts"/> times.
+		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
+		///</summary>
+		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
+		/// <param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+		public static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, int retryAttempts, CancellationToken cancellationToken)
+		{
+			return await dbCommand.ExecuteReaderWithRetryAsync(retryAttempts, _firstAttempt, cancellationToken);
+		}
+
+		private static async Task<DbDataReader> ExecuteReaderWithRetryAsync(this DbCommand dbCommand, int retryAttempts, int attemptNumber, CancellationToken cancellationToken = default)
 		{
 			try
 			{
-				return await dbCommand.ExecuteReaderAsync();
+				return await dbCommand.ExecuteReaderAsync(cancellationToken);
 			}
 			catch (Exception)
 			{
 				if (retryAttempts <= 0) throw;
 
-				await Task.Delay(((int)Math.Pow(2, Math.Min(attemptNumber, _maxExponent)) * 1000));
-				return await ExecuteReaderWithRetryAsync(dbCommand, retryAttempts--, attemptNumber++);
+				await Task.Delay((int)Math.Pow(2, Math.Min(attemptNumber, _maxExponent)) * 1000, cancellationToken: cancellationToken);
+				return await dbCommand.ExecuteReaderWithRetryAsync(retryAttempts--, attemptNumber++, cancellationToken);
 			}
 		}
 	}
