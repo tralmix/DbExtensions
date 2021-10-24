cbd484e78df9a3bd403e5c561b82d4e7081268ba
Alex Uhling <alex.uhling@gmail.com>
2021-10-24 10:14:00 -0400

    Added Async example of extension method use

--- examples/AsyncExample/Program.cs
+++ examples/AsyncExample/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using System.Data.Common;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 
 namespace AsyncExample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static readonly string inMemoryConnectionString = "Data Source=InMemorySample;Mode=Memory;Cache=Shared";
+		static async Task Main(string[] args)
+		{
+			using var connection = new SqliteConnection(inMemoryConnectionString);
+
+			await connection.EnsureOpenWithRetryAsync(2);
+			
+			using var command = connection.CreateCommand();
+
+			using var reader = await command.ExecuteReaderWithRetryAsync(2);
+
+			Debug.Assert(true);
+		}
+	}
 }
