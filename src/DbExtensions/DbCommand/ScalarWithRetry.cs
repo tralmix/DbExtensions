6997cd41ed609ee2a21c41d0c9a331e21fcdbaa8
Alex Uhling <alex.uhling@gmail.com>
2021-11-07 12:46:03 -0500

    Added nullable referernce type support to scalar functions when targeting  .Net 5.0.

--- src/DbExtensions/DbCommand/ScalarWithRetry.cs
+++ src/DbExtensions/DbCommand/ScalarWithRetry.cs
@@ -14,6 +14,43 @@ namespace System.Data.Common
 		private static readonly int _maxExponent = 8;
 
 		#region Synchronous
+#if NET5_0_OR_GREATER
+#nullable enable
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalar()"/> with one retry attempt.
+		///</summary>
+		///<returns>The first column of the first row in the result set.</returns>
+		public static object? ExecuteScalarWithRetry(this DbCommand dbCommand)
+		{
+			return dbCommand.ExecuteScalarWithRetry(_defaultRetryAttempts, _firstAttempt);
+		}
+
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalar()"/> up to <paramref name="retryAttempts"/> times.
+		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
+		///</summary>
+		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
+		///<returns>The first column of the first row in the result set.</returns>
+		public static object? ExecuteScalarWithRetry(this DbCommand dbCommand, int retryAttempts)
+		{
+			return dbCommand.ExecuteScalarWithRetry(retryAttempts, _firstAttempt);
+		}
+
+		private static object? ExecuteScalarWithRetry(this DbCommand dbCommand, int retryAttempts, int attemptNumber)
+		{
+			try
+			{
+				return dbCommand.ExecuteScalar();
+			}
+			catch (Exception)
+			{
+				if (retryAttempts <= 0) throw;
+
+				Task.Delay(((int)Math.Pow(2, Math.Min(attemptNumber, _maxExponent))) * 1000).Wait();
+				return dbCommand.ExecuteScalarWithRetry(retryAttempts--, attemptNumber++);
+			}
+		}
+#elif NETSTANDARD1_2_OR_GREATER
 		///<summary>
 		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalar()"/> with one retry attempt.
 		///</summary>
@@ -48,9 +85,73 @@ namespace System.Data.Common
 				return dbCommand.ExecuteScalarWithRetry(retryAttempts--, attemptNumber++);
 			}
 		}
+#endif
 		#endregion
 
 		#region Async
+#if NET5_0_OR_GREATER
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
+		///</summary>
+		///<returns>The first column of the first row in the result set.</returns>
+		///<exception cref="DbException">An error occurred while executing the command text.</exception>
+		public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand)
+		{
+			return await dbCommand.ExecuteScalarWithRetryAsync(_defaultRetryAttempts);
+		}
+
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
+		///</summary>
+		///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+		///<returns>The first column of the first row in the result set.</returns>
+		///<exception cref="DbException">An error occurred while executing the command text.</exception>
+		public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, CancellationToken cancellationToken)
+		{
+			return await dbCommand.ExecuteScalarWithRetryAsync(_defaultRetryAttempts, cancellationToken);
+		}
+
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
+		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
+		///</summary>
+		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
+		///<returns>The first column of the first row in the result set.</returns>
+		///<exception cref="DbException">An error occurred while executing the command text.</exception>
+		public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
+		{
+			return await dbCommand.ExecuteScalarWithRetryAsync(retryAttempts, _firstAttempt);
+		}
+
+		///<summary>
+		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
+		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
+		///</summary>
+		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
+		///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+		///<returns>The first column of the first row in the result set.</returns>
+		///<exception cref="DbException">An error occurred while executing the command text.</exception>
+		public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts, CancellationToken cancellationToken)
+		{
+			return await dbCommand.ExecuteScalarWithRetryAsync(retryAttempts, _firstAttempt, cancellationToken);
+		}
+
+		private static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts, int attemptNumber, CancellationToken cancellationToken = default)
+		{
+			try
+			{
+				return await dbCommand.ExecuteScalarAsync(cancellationToken);
+			}
+			catch (Exception)
+			{
+				if (retryAttempts <= 0) throw;
+
+				await Task.Delay((int)Math.Pow(2, Math.Min(attemptNumber, _maxExponent)) * 1000, cancellationToken: cancellationToken);
+				return await dbCommand.ExecuteScalarWithRetryAsync(retryAttempts--, attemptNumber++, cancellationToken);
+			}
+		}
+#nullable enable
+#elif NETSTANDARD1_2_OR_GREATER
 		///<summary>
 		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
 		///</summary>
@@ -111,6 +212,7 @@ namespace System.Data.Common
 				return await dbCommand.ExecuteScalarWithRetryAsync(retryAttempts--, attemptNumber++, cancellationToken);
 			}
 		}
+#endif
 		#endregion
 	}
 }
