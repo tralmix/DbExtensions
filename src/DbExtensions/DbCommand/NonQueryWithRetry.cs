5a1715ce0bb9cfbcd8100f48c36e1d6e29f93ef6
Alex Uhling <alex.uhling@gmail.com>
2021-11-17 21:44:32 -0500

    Massive overhaul of retry methods

--- src/DbExtensions/DbCommand/NonQueryWithRetry.cs
+++ src/DbExtensions/DbCommand/NonQueryWithRetry.cs
@@ -5,19 +5,9 @@ namespace System.Data.Common
 {
     public static class NonQueryWithRetry
     {
-        private static readonly int _defaultRetryAttempts = 1;
-        private static readonly int _firstAttempt = 1;
-        private static readonly int _maxExponent = 8;
-
-        #region Synchronous
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteNonQuery()"/> with one retry attempt.
-        ///</summary>
-        ///<returns>The number of rows affected.</returns>
-        public static int ExecuteNonQueryWithRetry(this DbCommand dbCommand)
-        {
-            return dbCommand.ExecuteNonQueryWithRetry(_defaultRetryAttempts);
-        }
+        private const int _defaultRetryAttempts = 1;
+        private const int _firstAttempt = 1;
+        private const int _maxExponent = 8;
 
         ///<summary>
         /// Attempts to run <see cref="DbCommand.ExecuteNonQuery()"/> up to <paramref name="retryAttempts"/> times.
@@ -25,7 +15,7 @@ namespace System.Data.Common
         ///</summary>
         ///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
         ///<returns>The number of rows affected.</returns>
-        public static int ExecuteNonQueryWithRetry(this DbCommand dbCommand, int retryAttempts)
+        public static int ExecuteNonQueryWithRetry(this DbCommand dbCommand, int retryAttempts = _defaultRetryAttempts)
         {
             var attempt = _firstAttempt;
             while (true)
@@ -40,41 +30,6 @@ namespace System.Data.Common
                     if (attempt > retryAttempts) throw;
                 }
         }
-        #endregion
-
-        #region Async
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteNonQueryAsync()"/> with one retry attempt.
-        ///</summary>
-        ///<returns>The number of rows affected.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<int> ExecuteNonQueryWithRetryAsync(this DbCommand dbCommand)
-        {
-            return await dbCommand.ExecuteNonQueryWithRetryAsync(_defaultRetryAttempts);
-        }
-
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteNonQueryAsync()"/> with one retry attempt.
-        ///</summary>
-        ///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
-        ///<returns>The number of rows affected.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<int> ExecuteNonQueryWithRetryAsync(this DbCommand dbCommand, CancellationToken cancellationToken)
-        {
-            return await dbCommand.ExecuteNonQueryWithRetryAsync(_defaultRetryAttempts, cancellationToken);
-        }
-
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteNonQueryAsync()"/> up to <paramref name="retryAttempts"/> times.
-        /// Calls will back off exponentionally at a rate of 2^n up to n=8.
-        ///</summary>
-        ///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
-        ///<returns>The number of rows affected.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<int> ExecuteNonQueryWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
-        {
-            return await dbCommand.ExecuteNonQueryWithRetryAsync(retryAttempts);
-        }
 
         ///<summary>
         /// Attempts to run <see cref="DbCommand.ExecuteNonQueryAsync()"/> up to <paramref name="retryAttempts"/> times.
@@ -84,7 +39,7 @@ namespace System.Data.Common
         ///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
         ///<returns>The number of rows affected.</returns>
         ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<int> ExecuteNonQueryWithRetryAsync(this DbCommand dbCommand, int retryAttempts, CancellationToken cancellationToken)
+        public static async Task<int> ExecuteNonQueryWithRetryAsync(this DbCommand dbCommand, int retryAttempts = _defaultRetryAttempts, CancellationToken cancellationToken = default)
         {
             var attempt = _firstAttempt;
             while (true)
@@ -99,6 +54,5 @@ namespace System.Data.Common
                     if (attempt > retryAttempts) throw;
                 }
         }
-        #endregion
     }
 }
--- src/DbExtensions/DbCommand/ScalarWithRetry.cs
+++ src/DbExtensions/DbCommand/ScalarWithRetry.cs
@@ -5,11 +5,10 @@ namespace System.Data.Common
 {
     public static class ScalarWithRetry
     {
-        private static readonly int _defaultRetryAttempts = 1;
-        private static readonly int _firstAttempt = 1;
-        private static readonly int _maxExponent = 8;
+        private const int _defaultRetryAttempts = 1;
+        private const int _firstAttempt = 1;
+        private const int _maxExponent = 8;
 
-        #region Synchronous
 #if NET5_0_OR_GREATER
 #nullable enable
         ///<summary>
@@ -42,23 +41,38 @@ namespace System.Data.Common
                     if (attempt > retryAttempts) throw;
                 }
         }
-#elif NETSTANDARD1_2_OR_GREATER
-		///<summary>
-		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalar()"/> with one retry attempt.
-		///</summary>
-		///<returns>The first column of the first row in the result set.</returns>
-		public static object ExecuteScalarWithRetry(this DbCommand dbCommand)
-		{
-			return dbCommand.ExecuteScalarWithRetry(_defaultRetryAttempts);
-		}
 
+        ///<summary>
+        /// Attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
+        /// Calls will back off exponentionally at a rate of 2^n up to n=8.
+        ///</summary>
+        ///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
+        ///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
+        ///<returns>The first column of the first row in the result set.</returns>
+        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
+        public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts = _defaultRetryAttempts, CancellationToken cancellationToken = default)
+        {
+            var attempt = _firstAttempt;
+            while (true)
+                try
+                {
+                    return await dbCommand.ExecuteScalarAsync(cancellationToken);
+                }
+                catch (Exception)
+                {
+                    await Task.Delay(((int)Math.Pow(2, Math.Min(attempt, _maxExponent))) * 1000, cancellationToken);
+                    attempt++;
+                    if (attempt > retryAttempts) throw;
+                }
+        }
+#elif NETSTANDARD1_2_OR_GREATER
 		///<summary>
 		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalar()"/> up to <paramref name="retryAttempts"/> times.
 		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
 		///</summary>
 		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
 		///<returns>The first column of the first row in the result set.</returns>
-		public static object ExecuteScalarWithRetry(this DbCommand dbCommand, int retryAttempts)
+		public static object ExecuteScalarWithRetry(this DbCommand dbCommand, int retryAttempts = _defaultRetryAttempts)
 		{
             var attempt = _firstAttempt;
             while (true)
@@ -73,101 +87,6 @@ namespace System.Data.Common
                     if (attempt > retryAttempts) throw;
                 }
         }
-#endif
-        #endregion
-
-        #region Async
-#if NET5_0_OR_GREATER
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
-        ///</summary>
-        ///<returns>The first column of the first row in the result set.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand)
-        {
-            return await dbCommand.ExecuteScalarWithRetryAsync(_defaultRetryAttempts);
-        }
-
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
-        ///</summary>
-        ///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
-        ///<returns>The first column of the first row in the result set.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, CancellationToken cancellationToken)
-        {
-            return await dbCommand.ExecuteScalarWithRetryAsync(_defaultRetryAttempts, cancellationToken);
-        }
-
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
-        /// Calls will back off exponentionally at a rate of 2^n up to n=8.
-        ///</summary>
-        ///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
-        ///<returns>The first column of the first row in the result set.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
-        {
-            return await dbCommand.ExecuteScalarWithRetryAsync(retryAttempts);
-        }
-
-        ///<summary>
-        /// Attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
-        /// Calls will back off exponentionally at a rate of 2^n up to n=8.
-        ///</summary>
-        ///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
-        ///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
-        ///<returns>The first column of the first row in the result set.</returns>
-        ///<exception cref="DbException">An error occurred while executing the command text.</exception>
-        public static async Task<object?> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts, CancellationToken cancellationToken = default)
-        {
-            var attempt = _firstAttempt;
-            while (true)
-                try
-                {
-                    return await dbCommand.ExecuteScalarAsync(cancellationToken);
-                }
-                catch (Exception)
-                {
-                    await Task.Delay(((int)Math.Pow(2, Math.Min(attempt, _maxExponent))) * 1000, cancellationToken);
-                    attempt++;
-                    if (attempt > retryAttempts) throw;
-                }
-        }
-#nullable enable
-#elif NETSTANDARD1_2_OR_GREATER
-		///<summary>
-		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
-		///</summary>
-		///<returns>The first column of the first row in the result set.</returns>
-		///<exception cref="DbException">An error occurred while executing the command text.</exception>
-		public static async Task<object> ExecuteScalarWithRetryAsync(this DbCommand dbCommand)
-		{
-			return await dbCommand.ExecuteScalarWithRetryAsync(_defaultRetryAttempts);
-		}
-
-		///<summary>
-		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> with one retry attempt.
-		///</summary>
-		///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
-		///<returns>The first column of the first row in the result set.</returns>
-		///<exception cref="DbException">An error occurred while executing the command text.</exception>
-		public static async Task<object> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, CancellationToken cancellationToken)
-		{
-			return await dbCommand.ExecuteScalarWithRetryAsync(_defaultRetryAttempts, cancellationToken);
-		}
-
-		///<summary>
-		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
-		/// Calls will back off exponentionally at a rate of 2^n up to n=8.
-		///</summary>
-		///<param name="retryAttempts">Number of attempts before thrown exception is thrown to caller.</param>
-		///<returns>The first column of the first row in the result set.</returns>
-		///<exception cref="DbException">An error occurred while executing the command text.</exception>
-		public static async Task<object> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts)
-		{
-			return await dbCommand.ExecuteScalarWithRetryAsync(retryAttempts);
-		}
 
 		///<summary>
 		/// Recursively attempts to run <see cref="DbCommand.ExecuteScalarAsync()"/> up to <paramref name="retryAttempts"/> times.
@@ -177,7 +96,7 @@ namespace System.Data.Common
 		///<param name="cancellationToken">A token to cancel the asynchronous operation.</param>
 		///<returns>The first column of the first row in the result set.</returns>
 		///<exception cref="DbException">An error occurred while executing the command text.</exception>
-		public static async Task<object> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts, CancellationToken cancellationToken)
+		public static async Task<object> ExecuteScalarWithRetryAsync(this DbCommand dbCommand, int retryAttempts = _defaultRetryAttempts, CancellationToken cancellationToken = default)
         {
             var attempt = _firstAttempt;
             while (true)
@@ -193,6 +112,5 @@ namespace System.Data.Common
                 }
         }
 #endif
-        #endregion
     }
 }
